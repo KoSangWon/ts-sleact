4ef397ec23a9abe47c6638485bf6f0c4a45bc1c8
Sangwon <jackoss@naver.com>
2021-08-14 22:50:41 +0900

    feat: add profile modal

---
+++ components/Menu/index.tsx
@@ -0,0 +1,31 @@
+import React, { CSSProperties, FC, useCallback } from 'react';
+import { CloseModalButton, CreateMenu } from './styles';
+
+interface Props {
+  show: boolean;
+  onCloseModal: () => void;
+  style: CSSProperties;
+  closeButton?: boolean;
+}
+
+const Menu: FC<Props> = ({ children, style, onCloseModal, show, closeButton }) => {
+  // 버블링 방지해서 부모 클릭하면 닫히도록 함. 모달을 클릭하면 bubbling 막아주기
+  const stopPropagation = useCallback((e) => {
+    e.stopPropagation();
+  }, []);
+
+  return (
+    <CreateMenu onClick={onCloseModal}>
+      <div style={style} onClick={stopPropagation}>
+        {closeButton && <CloseModalButton onClick={onCloseModal}>&times;</CloseModalButton>}
+        {children}
+      </div>
+    </CreateMenu>
+  );
+};
+
+Menu.defaultProps = {
+  closeButton: true,
+};
+
+export default Menu;
--- layouts/Workspace/index.tsx
+++ layouts/Workspace/index.tsx
@@ -4,6 +4,7 @@ import {
   Header,
   MenuScroll,
   ProfileImg,
+  ProfileModal,
   RightMenu,
   WorkspaceName,
   Workspaces,
@@ -11,15 +12,17 @@ import {
 } from '@layouts/Workspace/styles';
 import fetcher from '@utils/fetcher';
 import axios from 'axios';
-import React, { FC, useCallback } from 'react';
+import React, { useCallback, useState } from 'react';
 import { Redirect, Route, Switch } from 'react-router';
 import useSWR from 'swr';
 import gravatar from 'gravatar';
 import Channel from '@pages/Channel';
 import DirectMessage from '@pages/DirectMessage';
+import Menu from '@components/Menu';
 
 //children을 쓰려면 FC로 반환 타입 지정해주기
 const Workspace = () => {
+  const [showUserMenu, setShowUserMenu] = useState(false);
   const { data: userData, error, revalidate, mutate } = useSWR('/api/users', fetcher);
 
   const onLogOut = useCallback(() => {
@@ -33,6 +36,10 @@ const Workspace = () => {
       });
   }, []);
 
+  const onClickUserProfile = useCallback(() => {
+    setShowUserMenu((prev) => !prev);
+  }, []);
+
   if (!userData) {
     return <Redirect to="/login" />;
   }
@@ -41,11 +48,22 @@ const Workspace = () => {
     <div>
       <Header>Test</Header>
       <RightMenu>
-        <span>
+        <span onClick={onClickUserProfile}>
           <ProfileImg src={gravatar.url(userData.email, { s: '28px', d: 'retro' })} alt={userData.email} />
+          {showUserMenu && (
+            <Menu style={{ right: 0, top: 38 }} show={showUserMenu} onCloseModal={onClickUserProfile}>
+              <ProfileModal>
+                <img src={gravatar.url(userData.email, { s: '36px', d: 'retro' })} alt={userData.email} />
+                <div>
+                  <span id="profile-name">{userData.nickname}</span>
+                  <span id="profile-active">Active</span>
+                </div>
+              </ProfileModal>
+              <button onClick={onLogOut}>로그아웃</button>
+            </Menu>
+          )}
         </span>
       </RightMenu>
-      <button onClick={onLogOut}>로그아웃</button>
       <WorkspaceWrapper>
         <Workspaces>test</Workspaces>
         <Channels>
