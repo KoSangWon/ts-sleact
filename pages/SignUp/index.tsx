c3cd077f58930a8ef931990d2cff6ce5ef508ad3
Sangwon <jackoss@naver.com>
2021-08-14 17:29:31 +0900

    feat: add login page

--- pages/SignUp/index.tsx
+++ pages/SignUp/index.tsx
@@ -3,6 +3,7 @@ import { Button, Error, Form, Header, Input, Label, LinkContainer, Success } fro
 import useInput from '@hooks/useInput';
 import { Redirect } from 'react-router';
 import axios from 'axios';
+import { Link } from 'react-router-dom';
 
 const SignUp = () => {
   const [signUpError, setSignUpError] = useState('');
@@ -97,7 +98,7 @@ const SignUp = () => {
       </Form>
       <LinkContainer>
         이미 회원이신가요?&nbsp;
-        <a href="/login">로그인 하러가기</a>
+        <Link to="/login">로그인 하러가기</Link>
       </LinkContainer>
     </div>
   );
